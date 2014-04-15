d28a54117a958627e617220df70d223f9c933ff3
Denis Polish <denis.polish@worldapp.com>
2014-04-15 12:26:50 -0400

    city added

---
+++ src/City.java
@@ -0,0 +1,14 @@
+/**
+ * Created by denis.polish on 4/15/2014.
+ */
+public class City {
+    private String cityName;
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
